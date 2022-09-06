c3a7fbd03fd85bd7f9440cbb5bbfc3952dcd5091
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-09-06 20:04:55 +0530

    Add logic for return to main screen for start new game

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import Colors from "./utils/colors";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGamerIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const [fontsLoaded] = useFonts({
     "open-sans": require("./assets/Fonts/OpenSans-Regular.ttf"),
@@ -31,6 +32,11 @@ export default function App() {
     setGamerIsOver(true);
   }
 
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
@@ -39,7 +45,13 @@ export default function App() {
     );
   }
   if (gameIsOver && userNumber) {
-    screen = <GameOverScreen />;
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
   }
 
   return (
